6fc90ced09b9e25c0d1d79534c427862c650a4d6
Julian Gomez <julianmjadi@gmail.com>
2023-09-17 20:51:53 +0200

    Terminadas las pantallas de lista de reproduccion de cada genero disponible

--- Js/main.js
+++ Js/main.js
@@ -5,6 +5,13 @@ const imagenTres = document.getElementById("img-3");
 const imagenCuatro = document.getElementById("img-4");
 const imagenCinco = document.getElementById("img-5");
 const imagenSeis = document.getElementById("img-6");
+const listReggaeton = document.querySelector(".lista-reproduccion-reggaeton");
+const listElectronica = document.querySelector(".lista-reproduccion-electronica");
+const listBachata = document.querySelector(".lista-reproduccion-bachata");
+const listvallenato = document.querySelector(".lista-reproduccion-vallenato");
+const listSalsa = document.querySelector(".lista-reproduccion-salsa");
+const listRock = document.querySelector(".lista-reproduccion-rock");
+const cambiaColorCuatro = document.querySelector(".cambiacolor-4")
 
 
 
@@ -16,14 +23,22 @@ const siguiente = document.querySelector(".contenedor-siguiente");
 // EVENTOS
 atras.addEventListener('click', moverAtras);
 siguiente.addEventListener('click', moverAdelante);
+imagenDos.addEventListener('click', cancionesReggaeton);
+imagenUno.addEventListener('click', cancionesElectronica);
+imagenTres.addEventListener('click', cancionesBachata);
+imagenCuatro.addEventListener('click', cancionesVallenato);
+imagenCinco.addEventListener('click', cancionesSalsa);
+imagenSeis.addEventListener('click', cancionesRock);
+
+
+
 
 //POSICION ACTUAL DEL CARRUSEL
 let posicionActual = 0;
 
 
 //FUNCIONES
-
-function moverAtras() {
+function moverAdelante() {
     // RESTA 50% A LA POSICION ACTUAL 
     if (posicionActual > -300){
         posicionActual -= 50;
@@ -37,7 +52,7 @@ function moverAtras() {
     };
 };
 
-function moverAdelante() {
+function moverAtras() {
     if(posicionActual < 0){
         posicionActual += 50;
         imagenUno.style.transform = `translateX(${posicionActual}%)`;
@@ -48,3 +63,27 @@ function moverAdelante() {
         imagenSeis.style.transform = `translateX(${posicionActual}%)`;
     };
 };
+
+function cancionesReggaeton() {
+    listReggaeton.classList.toggle("activo");
+  }
+
+function cancionesElectronica() {
+    listElectronica.classList.toggle("activo");
+  }
+
+function cancionesBachata() {
+    listBachata.classList.toggle("activo");
+  }
+
+function cancionesVallenato() {
+    listvallenato.classList.toggle("activo");
+}  
+
+function cancionesSalsa() {
+    listSalsa.classList.toggle("activo");
+}
+
+function cancionesRock() {
+    listRock.classList.toggle("activo");
+}
